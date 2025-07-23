0ca0c103f999b88752aa08759198edd9b1080c54
Krutik090 <krutikthakar2539@gmail.com>
2025-07-23 11:17:40 +0530

    fix dashboard render issue

--- src/components/Dashboard/AnalyticsUserAction.jsx
+++ src/components/Dashboard/AnalyticsUserAction.jsx
@@ -41,11 +41,6 @@ export default function AnalyticsUserAction({ darkMode = false }) {
     theme={chartTheme}
   />
 
-
-  useEffect(() => {
-    fetchQuizCompletedData();
-  }, []);
-
   return (
     <div
       style={{
